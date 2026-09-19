2aacaa9e58ff62da55c5f802695f83906feb8959
ntybln33 <ntybln33@gmail.com>
2026-09-19 13:30:04 -0500

    response printed from ai model

--- main.py
+++ main.py
@@ -7,11 +7,25 @@ load_dotenv()
 api_key = os.environ.get("OPENROUTER_API_KEY")
 
 # api_key check
-if isinstance(api_key, None):
+if api_key is None:
     raise RuntimeError("api_key is none")
 
 ## creating client ##
 client = OpenAI(
-    base_url = "https://openrouter.ai/api/v1"
-    api_key = api_key
+    base_url = "https://openrouter.ai/api/v1",
+    api_key = api_key,
 )
+
+
+## calling response from model ##
+response = client.chat.completions.create(
+    model = "openrouter/free",
+    messages=[
+        {
+            "role": "user",
+            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+        }
+    ],
+)
+
+print(response.choices[0].message.content)
