269b2d943f97abe2947031dda228fe7292598145
ntybln33 <ntybln33@gmail.com>
2026-09-20 09:18:31 -0500

    Multiple Messages, added helper function to run response.

--- main.py
+++ main.py
@@ -1,31 +1,48 @@
+import argparse
 import os
+
 from dotenv import load_dotenv
 from openai import OpenAI
 
-
-load_dotenv()
-api_key = os.environ.get("OPENROUTER_API_KEY")
-
-# api_key check
-if api_key is None:
-    raise RuntimeError("api_key is none")
-
-## creating client ##
-client = OpenAI(
-    base_url = "https://openrouter.ai/api/v1",
-    api_key = api_key,
-)
+def generate_content(client, message):
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=message,
+    )
+    return response
 
 
-## calling response from model ##
-response = client.chat.completions.create(
-    model = "openrouter/free",
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="AI Code Assistant")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to the LLM")
+    args = parser.parse_args()
+
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if not api_key:
+        raise RuntimeError("OPENROUTER_API_KEY environment variable not set")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
     messages=[
-        {
-            "role": "user",
-            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-        }
-    ],
-)
+        {"role": "user", "content": args.user_prompt},
 
-print(response.choices[0].message.content)
+        ]
+
+    response = generate_content(client, messages)
+
+    if not response.usage:
+        raise RuntimeError("API response appears to be malformed")
+
+    print("Prompt tokens:", response.usage.prompt_tokens)
+    print("Response tokens:", response.usage.completion_tokens)
+    print("Response:")
+    print(response.choices[0].message.content)
+
+
+if __name__ == "__main__":
+    main()
